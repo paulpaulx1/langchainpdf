eaaf1b5b303ab8852273d36ac7a6c2b6a1391506
Paul Neenan <paulneenan@Pauls-MBP.lan>
2023-12-11 18:54:01 -0500

    chunking error must be fixed

--- app/chat/create_embeddings.py
+++ app/chat/create_embeddings.py
@@ -9,8 +9,7 @@ def create_embeddings_for_pdf(pdf_id: str, pdf_path: str):
     )
 
     loader = PyPDFLoader(pdf_path)
-
-    docs=loader.load_and_split(text_splitter)
+    docs = loader.load_and_split(text_splitter)
 
     for doc in docs:
         doc.metadata = {
@@ -20,21 +19,3 @@ def create_embeddings_for_pdf(pdf_id: str, pdf_path: str):
         }
 
     vector_store.add_documents(docs)
-
-    """
-    Generate and store embeddings for the given pdf
-
-    1. Extract text from the specified PDF.
-    2. Divide the extracted text into manageable chunks.
-    3. Generate an embedding for each chunk.
-    4. Persist the generated embeddings.
-
-    :param pdf_id: The unique identifier for the PDF.
-    :param pdf_path: The file path to the PDF.
-
-    Example Usage:
-
-    create_embeddings_for_pdf('123456', '/path/to/pdf')
-    """
-    
-    pass
