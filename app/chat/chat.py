3ba81fec0ac74c37b9b51e17a1b485a91e9cfc85
Paul Neenan <paulneenan@Pauls-MBP.lan>
2023-12-15 15:49:38 -0500

    langfuse tracing

--- app/chat/chat.py
+++ app/chat/chat.py
@@ -1,17 +1,20 @@
 import random
+from langchain.chat_models import ChatOpenAI
 from app.chat.models import ChatArgs
 from app.chat.vector_stores import retriever_map
 from app.chat.llms import llm_map
 from app.chat.memories import memory_map
 from app.chat.chains.retrieval import StreamingConversationalRetrievalChain
-from langchain.chat_models import ChatOpenAI
 from app.web.api import (
     set_conversation_components,
     get_conversation_components
 )
+from app.chat.score import random_component_by_score
+from app.chat.tracing.langfuse import langfuse
+from langfuse.model import CreateTrace
 
 def select_component(
-        component_type, component_map, chat_args
+    component_type, component_map, chat_args
 ):
     components = get_conversation_components(
         chat_args.conversation_id
@@ -22,9 +25,10 @@ def select_component(
         builder = component_map[previous_component]
         return previous_component, builder(chat_args)
     else:
-        random_name = random.choice(list(component_map.keys()))
+        random_name = random_component_by_score(component_type, component_map)
         builder = component_map[random_name]
         return random_name, builder(chat_args)
+
 
 def build_chat(chat_args: ChatArgs):
     retriever_name, retriever = select_component(
@@ -32,7 +36,6 @@ def build_chat(chat_args: ChatArgs):
         retriever_map,
         chat_args
     )
-
     llm_name, llm = select_component(
         "llm",
         llm_map,
@@ -43,21 +46,28 @@ def build_chat(chat_args: ChatArgs):
         memory_map,
         chat_args
     )
-
-    print(
-        f"running chain with memory: {memory_name}, llm: {llm_name}, retriever: {retriever_name}"
-    )
     set_conversation_components(
         chat_args.conversation_id,
         llm=llm_name,
         retriever=retriever_name,
         memory=memory_name
     )
+    
+
     condense_question_llm = ChatOpenAI(streaming=False)
+
+    trace = langfuse.trace(
+        CreateTrace(
+            id=chat_args.conversation_id,
+            metadata=chat_args.metadata
+        )
+    )
 
     return StreamingConversationalRetrievalChain.from_llm(
         llm=llm,
         condense_question_llm=condense_question_llm,
         memory=memory,
-        retriever=retriever
+        retriever=retriever,
+        callbacks=[trace.getNewHandler()],
+        metadata=chat_args.metadata
     )
